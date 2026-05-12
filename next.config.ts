7bcae483979e8809e8c5af84a31a13db0ad68df2
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2026-05-12 20:30:11 -0300

    feat: Add three to next.config

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   allowedDevOrigins: ['http://192.168.100.197'],
+  transpilePackages: ['three'],
 };
 
 const withNextIntl = createNextIntlPlugin();
