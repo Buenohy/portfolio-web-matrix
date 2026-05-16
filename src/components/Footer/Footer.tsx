1a584f718e8e69a39aa74cd69927ff81cb2124d9
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2026-05-16 20:29:00 -0300

    feat: Footer new colors

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import YinYangIcon from '../YinYangIcon/YinYangIcon';
 
 import { useTranslations } from 'next-intl';
 
@@ -9,9 +8,9 @@ import MatrixLogo from '@/../public/images/matrix-logo.png'
 export default function Footer() {
   const t = useTranslations('Footer');
   return (
-    <footer className="dark:bg-dark-black mb-20 bg-transparent px-5 text-center md:mb-0 lg:px-10">
+    <footer className="dark:bg-black-matrix mb-20 bg-transparent px-5 text-center md:mb-0 lg:px-10">
       <div className="flex max-w-7xl flex-col items-center gap-4 py-10 md:flex md:flex-row md:justify-between">
-        <p className="text-dark-black my-3 flex items-center justify-evenly gap-2 text-xs font-light md:flex md:gap-1 dark:text-white">
+        <p className="text-black-matrix my-3 flex items-center justify-evenly gap-2 text-xs font-light md:flex md:gap-1 dark:text-white-matrix">
           <Image 
           src={MatrixLogo}
           width={85} 
@@ -23,7 +22,7 @@ export default function Footer() {
             {t('nameCreator')}
           </Link>
         </p>
-        <p className="text-dark-black dark:text-white-pure my-3 text-xs font-light">
+        <p className="text-black-matrix dark:text-white-matrix my-3 text-xs font-light">
           {t('copyright')}
         </p>
       </div>
